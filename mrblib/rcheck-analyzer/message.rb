b77e1366b01c2a34941f9727a10652a328f9a494
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-10-17 08:51:33 -0700

    Raise argument error when json key was not found

--- mrblib/rcheck-analyzer/message.rb
+++ mrblib/rcheck-analyzer/message.rb
@@ -1,6 +1,8 @@
 module RcheckAnalyzer
   module Message
     def self.output_result analyze, multi_keys
+      raise ArgumentError, "invalid argument\n#{Error::USAGE}" if analyze.has_key? nil
+
       if multi_keys
         analyze.each {|k, v| pp [k, v.sort_by {|k, v| v }.reverse] }
       else

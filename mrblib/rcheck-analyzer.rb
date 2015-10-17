20d6787d8800444aef68a7f44fd6d95254e3e9a0
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-10-17 16:09:05 +0900

    Add sum option for multi keys

--- mrblib/rcheck-analyzer.rb
+++ mrblib/rcheck-analyzer.rb
@@ -2,6 +2,6 @@ def __main__(argv)
   if argv[1] == "version"
     puts "v#{RcheckAnalyzer::VERSION}"
   else
-    RcheckAnalyzer.run argv[1], argv[2].to_i, argv[3], argv[4]
+    RcheckAnalyzer.run argv[1], argv[2].to_i, argv[3], argv[4], argv[5]
   end
 end
--- mrblib/rcheck-analyzer/analyze.rb
+++ mrblib/rcheck-analyzer/analyze.rb
@@ -1,12 +1,12 @@
 module RcheckAnalyzer
-  def self.run log, total_line, key1, key2=nil
+  def self.run log, total_line, key1, key2=nil, type="count"
     data = []
     analyze = {}
     File.open(log, "r") do |file|
       Tail.new(file).each do |line|
         data << JSON.parse(line)
         if key1 == "keys"
-          pp data[0].keys
+          pp data[0]
           return
         end
         break if data.size > total_line
@@ -17,17 +17,27 @@ module RcheckAnalyzer
         if analyze[d[key1]].nil?
           analyze[d[key1]] = {}
         end
-        analyze[d[key1]][d[key2]] = 0 if analyze[d[key1]][d[key2]].nil?
-        analyze[d[key1]][d[key2]] += 1
+        if type == "sum"
+          if (key2_ary = key2.split(".")).size == 2
+            analyze[d[key1]][key2_ary[1]] = 0 if analyze[d[key1]][key2_ary[1]].nil?
+            analyze[d[key1]][key2_ary[1]] += d[key2_ary[0]][key2_ary[1]].to_f
+          else
+            analyze[d[key1]][key2] = 0 if analyze[d[key1]][key2].nil?
+            analyze[d[key1]][key2] += d[key2].to_f
+          end
+        else
+          analyze[d[key1]][d[key2]] = 0 if analyze[d[key1]][d[key2]].nil?
+          analyze[d[key1]][d[key2]] += 1
+        end
       else
         analyze[d[key1]] = 0 if analyze[d[key1]].nil?
         analyze[d[key1]] += 1
       end
     end
     if key2.nil?
-      analyze.sort_by {|k, v| v }.each {|a| puts a }
+      analyze.sort_by {|k, v| v }.reverse.each {|a| puts a }
     else
-      analyze.each {|k, v| puts [k, v] }
+      analyze.each {|k, v| pp [k, v.sort_by {|k, v| v }.reverse] }
     end
   end
 end

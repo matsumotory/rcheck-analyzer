c41d993606eadd79f72c90d2e7e71d4ed31d16ae
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-10-18 06:05:44 -0700

    Optimize memory usage

--- mrblib/rcheck-analyzer/analyze.rb
+++ mrblib/rcheck-analyzer/analyze.rb
@@ -21,34 +21,44 @@ module RcheckAnalyzer
     end
 
     def run
-      data = Data.data_from_log @log, @key1, @total_line
-      result = analyze_data data
+      result = {}
+      Data.each_data_from_log @log, @key1, @total_line do |line|
+        analyze_data_line line, result
+      end
       Message.output_result result, @multi_keys
+    end
+
+    def analyze_data_line d, analyze
+      create_analyze_data d, analyze
     end
 
     def analyze_data data
       analyze = {}
       data.each do |d|
-        if @key2.nil?
-          analyze[d[@key1]] = 0 if analyze[d[@key1]].nil?
-          analyze[d[@key1]] += 1
-        else
-          analyze[d[@key1]] = {} if analyze[d[@key1]].nil?
-          if @type == "sum"
-            if @key2_ary.nil?
-              analyze[d[@key1]][@key2] = 0 if analyze[d[@key1]][@key2].nil?
-              analyze[d[@key1]][@key2] += d[@key2].to_f
-            else
-              analyze[d[@key1]][@key2_ary[1]] = 0 if analyze[d[@key1]][@key2_ary[1]].nil?
-              analyze[d[@key1]][@key2_ary[1]] += d[@key2_ary[0]][@key2_ary[1]].to_f
-            end
-          else
-            analyze[d[@key1]][d[@key2]] = 0 if analyze[d[@key1]][d[@key2]].nil?
-            analyze[d[@key1]][d[@key2]] += 1
-          end
-        end
+        create_analyze_data d, analyze
       end
       analyze
+    end
+
+    def create_analyze_data d, analyze
+      if @key2.nil?
+        analyze[d[@key1]] = 0 if analyze[d[@key1]].nil?
+        analyze[d[@key1]] += 1
+      else
+        analyze[d[@key1]] = {} if analyze[d[@key1]].nil?
+        if @type == "sum"
+          if @key2_ary.nil?
+            analyze[d[@key1]][@key2] = 0 if analyze[d[@key1]][@key2].nil?
+            analyze[d[@key1]][@key2] += d[@key2].to_f
+          else
+            analyze[d[@key1]][@key2_ary[1]] = 0 if analyze[d[@key1]][@key2_ary[1]].nil?
+            analyze[d[@key1]][@key2_ary[1]] += d[@key2_ary[0]][@key2_ary[1]].to_f
+          end
+        else
+          analyze[d[@key1]][d[@key2]] = 0 if analyze[d[@key1]][d[@key2]].nil?
+          analyze[d[@key1]][d[@key2]] += 1
+        end
+      end
     end
   end
 end
--- mrblib/rcheck-analyzer/data.rb
+++ mrblib/rcheck-analyzer/data.rb
@@ -1,5 +1,26 @@
 module RcheckAnalyzer
   module Data
+    def self.each_data_from_log log, key, total_line
+      read_line = 0
+      data_line = nil
+      File.open(log, "r") do |file|
+        unless log == "/proc/self/fd/0"
+          fp = Tail.new file
+        else
+          fp = file
+        end
+        fp.each do |line|
+          if key == "keys"
+            pp JSON.parse(line)
+            exit 0
+          end
+          data_line = JSON.parse(line.downcase)
+          yield data_line
+          read_line += 1
+          break if total_line != 0 and read_line > total_line
+        end
+      end
+    end
     def self.data_from_log log, key, total_line
       data = []
       File.open(log, "r") do |file|

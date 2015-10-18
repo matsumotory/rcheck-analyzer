b627152b576db4da550d14ec6c58a7d0ce45b429
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-10-17 21:15:06 -0700

    Support Test

--- bintest/rcheck-analyzer.rb
+++ bintest/rcheck-analyzer.rb
@@ -1,12 +1,36 @@
 require 'open3'
 
 BIN_PATH = File.join(File.dirname(__FILE__), "../mruby/bin/rcheck-analyzer")
+LOG_PATH = File.join(File.dirname(__FILE__), "../resource.log")
 
-assert('hello') do
-  output, status = Open3.capture2(BIN_PATH)
+assert('keys') do
+  output, status = Open3.capture2(BIN_PATH, LOG_PATH, 0, "keys")
 
   assert_true status.success?, "Process did not exit cleanly"
-  assert_include output, "Hello World"
+  assert_include output, <<'KEYS'
+{"module"=>"mod_resource_checker",
+ "date"=>"sat oct 17 21:35:44 2015",
+ "type"=>"rcheckall",
+ "unit"=>nil,
+ "location"=>"/",
+ "remote_ip"=>"93.159.230.89",
+ "filename"=>"/usr/local/apache/htdocs/wiki/lib/exe/js.php",
+ "scheme"=>"http",
+ "method"=>"get",
+ "hostname"=>"wiki.matsumoto-r.jp",
+ "server_ip"=>"127.0.0.1",
+ "uri"=>"/lib/exe/js.php",
+ "real_server_name"=>"www.matsumoto-r.jp",
+ "uid"=>2,
+ "size"=>12503,
+ "content_length"=>0,
+ "status"=>200,
+ "pid"=>20047,
+ "threshold"=>nil,
+ "response_time"=>0,
+ "result"=>
+  {"rcheckucpu"=>0.007999, "rcheckscpu"=>0.003999, "rcheckmem"=>0.484375}}
+KEYS
 end
 
 assert('version') do
--- test/test_rcheck-analyzer.rb
+++ test/test_rcheck-analyzer.rb
@@ -1,6 +1,8 @@
 class TestRcheckAnalyzer < MTest::Unit::TestCase
+  LOG_PATH = File.join(File.dirname(__FILE__), "./resource.log")
   def test_main
-    assert_nil __main__([])
+    assert_equal({"wiki.matsumoto-r.jp"=>2},  __main__(["dummy", LOG_PATH, "1", "hostname"]))
+    assert_equal({"wiki.matsumoto-r.jp"=>{200=>4, 302=>1}, "blog.matsumoto-r.jp"=>{404=>4, 200=>63, 301=>1, 500=>2}, "moblog.matsumoto-r.jp"=>{200=>5, 301=>6, 404=>15}},  __main__(["dummy", LOG_PATH, "100", "hostname", "status"]))
   end
 end
 

b6d35f9021fc630701144ac185af18e2479f6819
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-10-18 01:26:56 -0700

    Add integration test

--- bintest/rcheck-analyzer.rb
+++ bintest/rcheck-analyzer.rb
@@ -1,10 +1,10 @@
 require 'open3'
 
 BIN_PATH = File.join(File.dirname(__FILE__), "../mruby/bin/rcheck-analyzer")
-LOG_PATH = File.join(File.dirname(__FILE__), "../resource.log")
+LOG_PATH = File.join(File.dirname(__FILE__), "../test/resource.log")
 
 assert('keys') do
-  output, status = Open3.capture2(BIN_PATH, LOG_PATH, 0, "keys")
+  output, status = Open3.capture2(BIN_PATH, LOG_PATH, "0", "keys")
 
   assert_true status.success?, "Process did not exit cleanly"
   assert_include output, <<'KEYS'
@@ -31,6 +31,42 @@ assert('keys') do
  "result"=>
   {"rcheckucpu"=>0.007999, "rcheckscpu"=>0.003999, "rcheckmem"=>0.484375}}
 KEYS
+end
+
+assert('hostname with one-line') do
+  output, status = Open3.capture2(BIN_PATH, LOG_PATH, "1", "hostname")
+
+  assert_true status.success?, "Process did not exit cleanly"
+  assert_include output, "[\"wiki.matsumoto-r.jp\", 2]\n"
+end
+
+assert('hostname') do
+  output, status = Open3.capture2(BIN_PATH, LOG_PATH, "100", "hostname")
+
+  assert_true status.success?, "Process did not exit cleanly"
+  assert_include output, "[\"blog.matsumoto-r.jp\", 70]\n[\"moblog.matsumoto-r.jp\", 26]\n[\"wiki.matsumoto-r.jp\", 5]\n"
+end
+
+assert('multipul keys') do
+  output, status = Open3.capture2(BIN_PATH, LOG_PATH, "100", "hostname", "status")
+
+  assert_true status.success?, "Process did not exit cleanly"
+  assert_include output, "[\"wiki.matsumoto-r.jp\", [[200, 4], [302, 1]]]\n[\"blog.matsumoto-r.jp\", [[200, 63], [404, 4], [500, 2], [301, 1]]]\n[\"moblog.matsumoto-r.jp\", [[404, 15], [301, 6], [200, 5]]]\n"
+end
+
+assert('sum with multipul keys') do
+  output, status = Open3.capture2(BIN_PATH, LOG_PATH, "100", "hostname", "result.rcheckucpu", "sum")
+
+  assert_true status.success?, "Process did not exit cleanly"
+  assert_include output, "[\"wiki.matsumoto-r.jp\", [[\"rcheckucpu\", 0.041994]]]\n[\"blog.matsumoto-r.jp\", [[\"rcheckucpu\", 16.506491]]]\n[\"moblog.matsumoto-r.jp\", [[\"rcheckucpu\", 5.637144]]]\n"
+end
+
+assert('sum with multipul keys using stdin') do
+  stdin_log, status = Open3.capture2("tail", "-n", "100", LOG_PATH)
+  output, status = Open3.capture2e(BIN_PATH, "stdin", "0", "hostname", "result.rcheckucpu", "sum", :stdin_data => stdin_log)
+
+  assert_true status.success?, "Process did not exit cleanly"
+  assert_include output, "[\"blog.matsumoto-r.jp\", [[\"rcheckucpu\", 16.107551]]]\n[\"moblog.matsumoto-r.jp\", [[\"rcheckucpu\", 5.637144]]]\n[\"wiki.matsumoto-r.jp\", [[\"rcheckucpu\", 0.041994]]]\n"
 end
 
 assert('version') do

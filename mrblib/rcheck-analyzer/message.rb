3713212fcbda5ed7caa6ad2479f9bf687453677b
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-10-18 03:55:47 -0700

    more argument error

--- mrblib/rcheck-analyzer/message.rb
+++ mrblib/rcheck-analyzer/message.rb
@@ -1,11 +1,15 @@
 module RcheckAnalyzer
   module Message
     def self.output_result analyze, multi_keys
-      raise ArgumentError, "invalid argument\n#{Error::USAGE}" if analyze.has_key? nil
 
       if multi_keys
-        analyze.each {|k, v| pp [k, v.sort_by {|k, v| v }.reverse] }
+        raise ArgumentError, "key1 is invalid argument\n#{Error::USAGE}" if analyze.has_key? nil
+        analyze.each do |k, v|
+          raise ArgumentError, "key2 is invalid argument\n#{Error::USAGE}" if v.has_key? nil
+          pp [k, v.sort_by {|k, v| v }.reverse]
+        end
       else
+        raise ArgumentError, "key1 is invalid argument\n#{Error::USAGE}" if analyze.has_key? nil
         analyze.sort_by {|k, v| v }.reverse.each {|a| puts a }
       end
       analyze

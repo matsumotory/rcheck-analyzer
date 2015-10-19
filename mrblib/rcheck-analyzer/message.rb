9842e6b916448636997a129839c6776641dc98b0
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-10-19 20:42:14 +0900

    Sort with value when using multipul keys

--- mrblib/rcheck-analyzer/message.rb
+++ mrblib/rcheck-analyzer/message.rb
@@ -6,8 +6,10 @@ module RcheckAnalyzer
         raise ArgumentError, "key1 is invalid argument\n#{Error::USAGE}" if analyze.has_key? nil
         analyze.each do |k, v|
           raise ArgumentError, "key2 is invalid argument\n#{Error::USAGE}" if v.has_key? nil
-          pp [k, v.sort_by {|k, v| v }.reverse]
         end
+        analyze.sort do |a, b|
+          a[1].to_a.flatten[1] <=> b[1].to_a.flatten[1]
+        end.reverse.each {|a| puts a }
       else
         raise ArgumentError, "key1 is invalid argument\n#{Error::USAGE}" if analyze.has_key? nil
         analyze.sort_by {|k, v| v }.reverse.each {|a| puts a }
